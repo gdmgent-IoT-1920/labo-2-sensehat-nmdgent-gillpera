74bb4181437c053ee3712d90b819f41e6efc1f1b
nmdgent-gillpera <gillian.peraltagutierrez@student.arteveldehs.be>
2020-03-04 13:52:22 +0100

    Update baseline.py
    #updated

--- baseline.py
+++ baseline.py
@@ -3,3 +3,8 @@ from sense_hat import SenseHat
 from random import randint
 sense = SenseHat()
 
+while True:
+    colour = (0,0,0)
+    randomBackgroundCol = (randint(0,255),randint(0,255),randint(0,255))
+    sense.show_message("Hello! We are New Media Development :)", text_colour= colour,back_colour=randomBackgroundCol)
+    sense.low_light = True
